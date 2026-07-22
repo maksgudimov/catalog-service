303a158feadc47e6eef53a50fe6474911f8a62e4
m.gudimov <maks.gudimov.90@mail.ru>
2026-07-22 15:51:23 +0300

    TASK-1001: заменил Fatal() на Println() при отсутствии .env файла

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -19,7 +19,7 @@ var Root Config
 
 func Load() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("No .env file found, using system environment variables")
 	}
 
 	if err := envconfig.Process("APP", &Root); err != nil {
