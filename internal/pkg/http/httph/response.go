b1beb6ca8c893f66d0f5fa36d81d12f6f4006cd9
m.gudimov <maks.gudimov.90@mail.ru>
2026-08-12 14:51:10 +0300

    TASK-1005: игнорирование ошибки в sendError и SendJSON

--- internal/pkg/http/httph/response.go
+++ internal/pkg/http/httph/response.go
@@ -14,7 +14,7 @@ type httpCoder interface {
 func SendJSON(w http.ResponseWriter, status int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(data)
+	_ = json.NewEncoder(w).Encode(data)
 }
 
 func SendEmpty(w http.ResponseWriter, status int) {
@@ -24,7 +24,7 @@ func SendEmpty(w http.ResponseWriter, status int) {
 func sendError(w http.ResponseWriter, status int, err error) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+	_ = json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
 }
 
 func HandleError(w http.ResponseWriter, err error) {
